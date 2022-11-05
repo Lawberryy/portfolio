4e7e3e535acc79fed6e21876047e66dd21236739
Sixtine Delvallée <sixtine.delvallee@gmail.com>
2022-11-05 16:34:46 +0100

    :sparkles: add projects section

---
+++ scroll-animations.js
@@ -0,0 +1,11 @@
+const anim = ScrollReveal({
+    reset: true,
+})
+
+anim.reveal('.portrait', {
+    origin: 'right',
+    distance: '50px',
+    duration: 2000,
+    // rotate: {x: 90, y: 30, z: 0}
+    delay: 1000
+})
